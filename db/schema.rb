9506708dee2a90ff28ce594d7161c013a985ad1e
Alison A Stewart <alisoninlife@gmail.com>
2018-12-09 16:59:17 -0500

    create indexes and more tables for functionality

--- db/schema.rb
+++ db/schema.rb
@@ -164,5 +164,109 @@ CREATE INDEX INDEX_EVENTS_ON_SCHOOL_ID_AND_EVENT ON EVENTS(SCHOOL_ID, EVENT_TYPE
 
   CREATE INDEX INDEX_PHRASES_ON_NODE_ID ON PHRASES (NODE_ID);     
 
-    
+  CREATE TABLE PRODUCTS (
+    NAME TEXT NOT NULL,
+    DESCRIPTION TEXT NOT NULL,
+    PLATFORM TEXT NOT NULL,
+    CREATED_AT TIMESTAMP,
+    UPDATED_AT TIMESTAMP,
+    VIDEO_URL TEXT,
+    PREVIEW_PHOTO TEXT,
+    POSITION INTEGER
+  );
+
+  CREATE TABLE PROGRAM_NOTES (
+    PROGRAM_SLUG TEXT,
+    SCHOOL_ID INTEGER NOT NULL,
+    TEXT TEXT NOT NULL,
+    CREATED_AT TIMESTAMP,
+    UPDATED_AT TIMESTAMP
+  );
+
+  CREATE TABLE SCHOOLS (
+    NAME TEXT NOT NULL,
+    CODE TEXT NOT NULL,
+    PARENT_CODE TEXT,
+    CREATED_AT TIMESTAMP,
+    UPDATED_AT TIMESTAMP,
+    LOGO_PHOTO TEXT,
+    GRAD_PROGRAM_CODE TEXT,
+    REGISTRATIONS_ENABLED BOOLEAN DEFAULT FALSE,
+    GRAD_GROUP_CODE TEXT,
+    SCHOOL_DAY_PROGRAM_CODE TEXT
+  );
+
+  CREATE UNIQUE INDEX INDEX_SCHOOLS_ON_CODE ON SCHOOLS (CODE);
+
+  CREATE TABLE SETTINGS (
+    KEY TEXT NOT NULL,
+    VALUE TEXT,
+    CREATED_AT TIMESTAMP,
+    UPDATED_AT TIMESTAMP
+  );
+
+  CREATE UNIQUE INDEX INDEX_SETTINGS_ON_KEY ON SETTINGS (KEY);
+
+
+  create_table "sittings", force: :cascade do |t|
+    t.string   "name",                               null: false
+    t.string   "duration",                           null: false
+    t.string   "sitting_fee"
+    t.string   "deposit"
+    t.integer  "event_type_id",                      null: false
+    t.string   "grad_program_code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "sitting_type",      default: "grad", null: false
+    t.integer  "school_id"
+    t.string   "grad_group_code"
+  end
+
+  create_table "time_trade_appointment_types", force: :cascade do |t|
+    t.integer  "time_trade_meta_data_id"
+    t.string   "type_id",                 null: false
+    t.string   "photo_type"
+    t.string   "resource_id",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "time_trade_appointment_types", ["time_trade_meta_data_id", "photo_type"], name: "index_appointment_types_on_meta_data_and_photo_type", unique: true, using: :btree
+
+  create_table "time_trade_imports", force: :cascade do |t|
+    t.string   "file",       null: false
+    t.text     "import_log"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "time_trade_meta_data", force: :cascade do |t|
+    t.string   "campaign_id"
+    t.string   "appointment_type_group_id"
+    t.string   "company_name"
+    t.string   "school_code",               null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "school_id"
+  end
+
+  add_index "time_trade_meta_data", ["school_id"], name: "index_time_trade_meta_data_on_school_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                                       null: false
+    t.string   "crypted_password",                            null: false
+    t.string   "salt",                                        null: false
+    t.string   "remember_token"
+    t.datetime "remember_token_expires_at"
+    t.boolean  "enabled"
+    t.string   "role",                      default: "admin", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "school_id"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+  add_foreign_key "time_trade_meta_data", "schools"
+end      
   
